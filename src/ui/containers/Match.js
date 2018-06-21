bee458b1ea5d941a3102fe454182d5b84ad55dea
ronny <ronnykwon@gmail.com>
2018-06-20 15:18:32 -1000

    moment added to handle text display for datetime

--- src/ui/containers/Match.js
+++ src/ui/containers/Match.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
 import './Match.css';
+import 'moment/locale/fr';
+import Moment from 'react-moment';
+
 class Match extends Component {
     
   render() {
+    const calendarStrings = {
+        lastDay : '[Hier à] LT',
+        sameDay : '[Aujourd\'hui à] LT',
+        nextDay : '[Demain à] LT',
+        lastWeek : 'dddd [dernier à] LT',
+        nextWeek : 'dddd [at] LT',
+        sameElse : 'L'
+    };
+
     let bgColor = 'bg-secondary';
     if(this.props.match.status === 'completed') {
         if(this.props.match.winner_code === this.props.code)
@@ -17,7 +29,7 @@ class Match extends Component {
         <div className="row match">
         <div className={`col-12 card text-center ${bgColor}`}>
             <div className="card-body">
-                <h5 className="card-title"><FontAwesome name="calendar"/>  {this.props.match.datetime}</h5>
+                <h5 className="card-title"><FontAwesome name="calendar"/>  <Moment locale="fr" calendar={calendarStrings}>{this.props.match.datetime}</Moment></h5>
                 <div className="card-text row">
                 <div className="col-5">
                 {this.props.match.home_team.country}
